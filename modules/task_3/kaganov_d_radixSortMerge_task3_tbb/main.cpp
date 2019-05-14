101a27ca6c16c150eab73959e9545ec8c8f2c92a
dmtrii-kg <dmitryi.kaganov@gmail.com>
2019-05-14 11:23:19 +0300

    edit lambda func, again

--- modules/task_3/kaganov_d_radixSortMerge_task3_tbb/main.cpp
+++ modules/task_3/kaganov_d_radixSortMerge_task3_tbb/main.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <cmath>
 #include <utility>
+#include <cstdlib>
 #include <iostream>
 
 unsigned int *sortMerge(unsigned int *firstArray, int firstSize,
@@ -116,7 +117,7 @@ unsigned int* radixSortParallel(unsigned int* A, unsigned int arrSize, int size,
     }
 
     tbb::task_scheduler_init init(numThreads);
-    tbb::parallel_for(tbb::blocked_range<int>(0, numThreads), [=, &R](const tbb::blocked_range<int> &thread) {
+    tbb::parallel_for(tbb::blocked_range<int>(0, numThreads), [=] (const tbb::blocked_range<int> &thread) {
         for (int i = thread.begin(); i != thread.end(); i++)
             lsdSort(R + i * size, sizeArr[i]);
     });
@@ -142,7 +143,7 @@ unsigned int* radixSortParallel(unsigned int* A, unsigned int arrSize, int size,
         }
         r2[block - 1] = arrSize - 1;
         init.initialize(block);
-        tbb::parallel_for(tbb::blocked_range<int>(0, block), [=, &R](const tbb::blocked_range<int> &thread) {
+        tbb::parallel_for(tbb::blocked_range<int>(0, block), [=] (const tbb::blocked_range<int> &thread) {
             for (int i = thread.begin(); i != thread.end(); i++) {
                 unsigned int* tmp = new unsigned int[r1[i] - l1[i] + 1 + r2[i] - l2[i] + 1];
                 tmp = sortMerge(R + l1[i], r1[i] - l1[i] + 1, R + l2[i], r2[i] - l2[i] + 1);
